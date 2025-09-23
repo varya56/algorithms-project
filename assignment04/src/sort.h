c1a3dae5896d9d95cbdba8a26564da4dc717ad15
Varvara Trakht <trakht.v@gmail.com>
2025-09-23 12:24:52 -0400

    Change code for assignment 4

--- assignment04/src/sort.h
+++ assignment04/src/sort.h
@@ -40,18 +40,48 @@ namespace csi281 {
   // using the bubble sort algorithm
   template <typename T> void bubbleSort(T array[], const int length) {
     // YOUR CODE HERE
+    bool swapped = true;
+    do {
+      swapped = false;
+      for (int i = 0; i < length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+          swap(array[i], array[i + 1]);
+          swapped = true;
+        }
+        }
+    }while (swapped);
   }
 
   // Performs an in-place ascending sort of *array* of size *length*
   // using the selection sort algorithm
   template <typename T> void selectionSort(T array[], const int length) {
     // YOUR CODE HERE
+    for (int i = 0; i < length - 1; i++) {
+      int minIndex = i;
+      for (int j = i + 1; j < length; j++) {
+        if (array[j] < array[minIndex]) {
+          minIndex = j;
+        }
+      }
+      if (minIndex != i) {
+        swap(array[i], array[minIndex]);
+      }
+    }
   }
 
   // Performs an in-place ascending sort of *array* of size *length*
   // using the insertion sort algorithm
   template <typename T> void insertionSort(T array[], const int length) {
     // YOUR CODE HERE
+    for (int i = 1; i < length; i++) {
+      T key = array[i];
+      int j = i - 1;
+      while (j >= 0 && array[j] > key) {
+        array[j + 1] = array[j];
+        j = j - 1;
+      }
+      array[j + 1] = key;
+    }
   }
 }  // namespace csi281
 
